8e5e882644ed8857508475d699bb5153cbe53693
obomatee <tonyoboma@gmail.com>
2023-06-29 22:16:37 -0600

    first commit

---
+++ Variables.java
@@ -0,0 +1,13 @@
+package org.comit.course;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int num = 5;
+		System.out.println("This in an integer" + " " + num);
+		
+
+	}
+
+}
